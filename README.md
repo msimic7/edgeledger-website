73e5cd980ceea71c5feea6886875fc732cc3a4b7
Stefan Petrovic <petrovic.stefan.rs@gmail.com>
2019-02-26 22:49:16 +0100

    small review this time

---
+++ README.md
@@ -0,0 +1,38 @@
+[](# Review Branch
+
+## Review 1
+***(26.02.2019)***
+
+---
+
+### Upotreba prethodnih saveta
+
+Ovde sve ok, primenjeno sve iz prosloIH review-a formatiranja, naime idalje ponegde spojis dve funkcije ili u css dva rule bloka spojis. Iako je to sve validan kod sjebava citljivost kada se poveca codebase. Najbolje zato na manjim projektima vezbati samokontrolu oko toga da udje u rutinu.
+
+---
+
+### CSS Selectors
+
+U prethodnom review sam pomenuo preprocessors i CSS metodologije. U principu oni ce kada ih predjes mnogo toga da razjasne i olaksaju, ali za pocetak treba da izbacis upotrebu ID-jeva za pisanje pravila u CSS-u.
+
+Iako je kao i sve ostalo sto sam "zamerio" validan kod. Pisanje ID-jeva moze da izazove razne nuspojave kada poraste projekat.
+
+Neke od negativnih strana su:
+	- THERE CAN ONLY BE ONE!!! Zamisli projekat za 20ak stranica i na svakoj po 30 ID-jeva, ko ce zapamtiti 600 cuda kako se tacno zovu da bi ih promenio?
+	- ID ime prioritet u odnosu na klasu - Dakle dovoljno da se jednom prejebes i napises unutar pravila koje si zalepio za ID nesto sto ce ti pokvari ceo layout.
+
+ID-jeve koristi u JS-u. Za to su odlicni i imaju smisla, za css najbolje sto pre izuciti neke od metodologija i primenjivati ili jednu metodologiju uvek ili se igrati sa njima, mesati i miksati ih ali drzati css skalabilnim.
+
+---
+
+### Ostatak
+
+Ostali feadback ti je u komentarima u kodu. U principu nikakvih zamerki samo mala skretanja paznje na ovo ono. Jedino obrati paznju na komentare u JS-u jer tu mozda budes imao pitanja.
+
+---
+
+Thats it, have a good one!
+<pre>
+ʕノ•ᴥ•ʔノ ︵ ┻━┻
+</pre>
+)
--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
+/*
+ * Using const without Babel is a big risk for backwards compatability
+ * Not all browsers support it
+ */
+
 function initMap() {
+  /*
+   * Use trailing comma in objects, this will reduce git diff footprint and keep git history cleaner
+   * on every subsequent commit if something was added to the object.
+    const loc = {
+     lat: 44.787197,
+	 lng: 20.457273
+	};
+   */
+
   const loc = {
-    lat: 44.787197, lng: 20.457273
+	lat: 44.787197,
+	lng: 20.457273
   };
-  const map = new google.maps.Map(document.querySelector('.map'),
-  {
+
+  const map = new google.maps.Map(document.querySelector('.map'), {
     zoom: 14,
     center: loc
   });
-  const marker = new google.maps.Marker ( { position: loc, map: map});
+
+  /* const marker = new google.maps.Marker ( { position: loc, map: map});
+   * No need to assign google.maps.Marker() to a const if its not going to be used
+   * anywhere else in the codebase
+   * See eslint no-unused-vars for more info
+   */
+  new google.maps.Marker({ position: loc, map: map});
 }
 
 window.addEventListener('scroll', function() {
@@ -18,17 +39,17 @@ window.addEventListener('scroll', function() {
   }
 });
 
+// This works only because you are loading main.js at the end of html DOM
+// More on this in Readme.md / It's a bad practice
+
 $('#navbar a, .btn').on('click', function(event) {
+  // NOTE: Make sure you are certain what *this* refers to, scoping can be tricky but here it's used correctly
   if(this.hash !== '') {
-    
     event.preventDefault();
     const hash = this.hash;
 
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top - 100
-      },
-      800
-      );
+    $('html, body').animate({
+     scrollTop: $(hash).offset().top - 100,
+	}, 800);
   }
 });
